d734ae6eb9fb1036bb1f2b06b864c823297cf195
Chang Liu <shampliu@gmail.com>
2016-05-30 15:51:36 -0700

    get set cookies

--- js/index.js
+++ js/index.js
@@ -1,10 +1,79 @@
-$(document).ready(function() {
-	$(".element-item").on('mouseover', function() {
-		this.style.cursor = "pointer";
-		$('.element-overlay').css('opacity', 1);
-	})
-	$(".element-item").on('mouseout', function() {
-		this.style.cursor = "pointer";
-		$('.element-overlay').css('opacity', 0);
-	})
-})
+function GetSetCookie() {
+  var version = getCookie("version");
+  if (version != null && version != "") {
+    if (version == 'full') {
+      version = 'text';
+      console.log('line 6');
+    }
+    else {
+      version = 'full';
+      console.log('line 10');
+    }
+  }
+  else {
+    version = 'full';
+    console.log('line 15');
+  }
+  setCookie("version", version, 365);
+  window.top.location.reload();
+}
+function setCookie(c_name, value, exdays) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+  document.cookie = c_name + "=" + c_value + "; path=/";
+}
+function getCookie(c_name) {
+  var i, x, y, ARRcookies = document.cookie.split(";");
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x == c_name) {
+      return unescape(y);
+    }
+  }
+}
+
+$(document).ready(function(){
+  GetSetCookie();
+
+  $('.login-button').on("click",function(e){
+    e.preventDefault;
+  });
+
+  $("#username").css("border-color", "green");
+  var type1 = function(callbackFunction){
+    $("#username").typed({
+      strings: ["Joe Bruin", "Josephine Bruin"],
+      typeSpeed: 14,
+      callback: function(){
+        callbackFunction();
+      },
+      showCursor: true,
+      // character for cursor
+      cursorChar: "|"
+    });
+  }
+  var type2 = function(){
+    $("#password").css("border-color", "green");
+    $("#password").typed({
+      strings: ["^400Congratulations Class of 2016!"],
+      typeSpeed: 14,
+      showCursor: true,
+          // character for cursor
+          cursorChar: "|",
+          callback: function(){
+            $(".logon-button").css("background-color", "yellow") 
+
+
+            window.location.replace("main.html");
+
+          },
+        });
+  }
+
+  type1(type2);
+
+
+});
