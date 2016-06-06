3b3e0362308b7623d7ac4574a728d863b306d308
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-06 02:25:41 -0700

    to avoid legal issues

--- js/main.js
+++ js/main.js
@@ -650,24 +650,24 @@ $(document).ready(function() {
 	});
 
 	/*for dropdown menu*/
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+	function myFunction() {
+		document.getElementById("myDropdown").classList.toggle("show");
+	}
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+	// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+		if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+			var dropdowns = document.getElementsByClassName("dropdown-content");
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('show')) {
+					openDropdown.classList.remove('show');
+				}
+			}
+		}
+	}
 
 	/*for updating date*/
 	function updateClock() {
@@ -690,8 +690,8 @@ window.onclick = function(event) {
 	  centerPadding: '140px',
 	  slidesToShow: 1,
 	  dots: true,
-	  //autoplay: true,
-	  //autoplaySpeed: 2000,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
 	  responsive: [
 	  {
 	    breakpoint: 990,
