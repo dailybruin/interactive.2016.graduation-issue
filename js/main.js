28e35a9eaf703d007a6ae4f686895d1eaacef718
Paulina Lei <paulina.y.lei@gmail.com>
2016-06-05 22:38:48 -0700

    magnific popup

--- js/main.js
+++ js/main.js
@@ -630,6 +630,26 @@ $(document).ready(function() {
 		});
 	});
 
+	/*magnific popup*/
+
+	$('.video').magnificPopup({
+		type: 'iframe',
+
+
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">'+
+			'<div class="mfp-close"></div>'+
+			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+			'<div class="mfp-title"></div>'+
+			'</div>'
+		},
+		callbacks: {
+			markupParse: function(template, values, item) {
+				values.title = item.el.attr('title');
+			}
+		}
+	});
+
 	/*for updating date*/
 	function updateClock() {
 		var now = new Date(),
